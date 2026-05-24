8b35b20c7b5f7e0016faade16c875e0c6e2c30cc
Marcus Vinicius Castelo Branco Martins <viniciuscastelo6@hotmail.com>
2026-05-24 17:42:01 -0300

    chore: align CLI version with package

--- src/cli.ts
+++ src/cli.ts
@@ -15,7 +15,7 @@ const program = new Command();
 program
   .name("wt")
   .description("Reusable Git worktree orchestration CLI")
-  .version("0.1.1");
+  .version("0.2.0");
 
 program
   .command("setup")
